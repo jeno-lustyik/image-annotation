e900e30d52ccb9e1785f344ac8cf74fcef85d448
Jeno Lustyik <lustyik.jeno@gmail.com>
2022-04-19 13:55:50 +0200

    color updates

--- image_annotation.py
+++ image_annotation.py
@@ -9,17 +9,17 @@ img_copy = img.copy()
 # Hair rectangle
 cv.rectangle(img_copy, (100,250), (400,25), (0,255,0))
 cv.rectangle(img_copy, (100, 25), (200, 0), (0, 255, 0), cv.FILLED)
-cv.putText(img_copy, 'Fry\'s Hair', (100, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+cv.putText(img_copy, 'Fry\'s Hair', (100, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
 
 # Coat rectangle
-cv.rectangle(img_copy, (100, 415), (370, 305), (255, 0, 0))
-cv.rectangle(img_copy, (100, 305), (200, 280), (255, 0, 0), cv.FILLED)
-cv.putText(img_copy, 'Fry\'s Coat', (100, 295), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+cv.rectangle(img_copy, (100, 415), (370, 305), (0, 0, 255))
+cv.rectangle(img_copy, (100, 305), (200, 280), (0, 0, 255), cv.FILLED)
+cv.putText(img_copy, 'Fry\'s Coat', (100, 295), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
 
 # Face rectangle
-cv.rectangle(img_copy, (230, 330), (390, 175), (255, 255, 0))
-cv.rectangle(img_copy, (230, 175), (330, 150), (255, 255, 0), cv.FILLED)
-cv.putText(img_copy, 'Fry\'s Face', (230, 165), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+cv.rectangle(img_copy, (230, 330), (390, 175), (0, 255, 255))
+cv.rectangle(img_copy, (230, 175), (330, 150), (0, 255, 255), cv.FILLED)
+cv.putText(img_copy, 'Fry\'s Face', (230, 165), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
 
 
 cv.imshow('image', img_copy)
